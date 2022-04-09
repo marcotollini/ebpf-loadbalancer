acc087f8612ac3c07251cf88132478d95bb1fb96
Marco <marco.tollini1@swisscom.com>
2022-04-09 11:55:02 +0200

    test

--- src/unyte_reuseport_kern.c
+++ src/unyte_reuseport_kern.c
@@ -110,8 +110,8 @@ enum sk_action _selector(struct sk_reuseport_md *reuse) {
   enum sk_action action;
   struct iphdr ip;
   u32 key;
-  int is_ipv4 = reuse->eth_protocol == 2054;
-  bpf_printk(LOC "IPv4=%d, is_ipv4=\n", reuse->eth_protocol, is_ipv4);
+  int is_ipv4 = reuse->eth_protocol == 0x0800;
+  bpf_printk(LOC "IPv4=%d, is_ipv4=%d\n", reuse->eth_protocol, is_ipv4);
 
   void *targets;
 
